4469aa11290aebc3b1b8a67e95d71216802f0f57
Ellen Cooper <ellencooper74@gmail.com>
2017-10-31 16:27:16 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import socketIOClient from "socket.io-client";
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      response: false,
+      endpoint: "http://127.0.0.1:8080"
+    };
+  }
+  componentDidMount() {
+    const { endpoint } = this.state;
+    const socket = socketIOClient(endpoint);
+    socket.on("FromAPI", data => this.setState({ response: data }));
+  }
+  render() {
+    const { response } = this.state;
+    return (
+      <div style={{ textAlign: "center" }}>
+        {response
+          ? <p>
+              The temperature in Florence is: {response} °F
+            </p>
+          : <p>Loading...</p>}
+      </div>
+    );
+  }
+}
+export default App;

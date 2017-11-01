0767e510dd26a2655c5590db3a059bebb354f412
Ellen Cooper <ellencooper74@gmail.com>
2017-11-01 16:02:20 -0600

    removes redundant code

--- src/App.js
+++ src/App.js
@@ -4,20 +4,7 @@ import socketIOClient from "socket.io-client";
 import OrderTable from './components/OrderTable'
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      response: false,
-      endpoint: "http://127.0.0.1:8080"
-    };
-  }
-  // componentDidMount() {
-  //   const { endpoint } = this.state;
-  //   const socket = socketIOClient(endpoint);
-  //   socket.on("FromAPI", data => this.setState({ response: data }));
-  // }
   render() {
-    const { response } = this.state;
     return (
       <div style={{ textAlign: "center" }}>
         <OrderTable type="bids"/>

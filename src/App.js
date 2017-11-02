6ab7f62b5c6b646902acdfe3ab9887cad68baf26
Ellen Cooper <ellencooper74@gmail.com>
2017-11-01 19:05:07 -0600

    removes redundant spacing and attributes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ class App extends Component {
       response: false,
       endpoint: "http://127.0.0.1:8080",
       orders: {},
-      exchanges: ["Bittrex", "Poloniex"]
     }
   }
   componentDidMount() {
@@ -24,25 +23,20 @@ class App extends Component {
         <h1 class = "text-center">Combined Order Books</h1><br></br>
         <h3 class = "text-center">BTC-ETH</h3><br></br>
         <div class="col-md-6 offset-md-3">
-
         <h5 class = "text-center font-italic bg-warning">Highlighting Represents Overlapping Asks/Bids</h5><br></br>
       </div>
-
         <div class="row">
           <div class="col-md-6">
             <h3 class = "text-center">Bids</h3>
             <OrderTable type="bids" orders={this.state.orders[0]}/>
-
           </div>
           <div class="col-md-6">
             <h3 class = "text-center">Asks</h3>
             <OrderTable type="asks" orders={this.state.orders[1]}/>
-
           </div>
         </div>
       </div>
     </div>
-
     )
   }
 }
--- src/components/OrderTable.js
+++ src/components/OrderTable.js
@@ -1,31 +1,17 @@
 import React, { Component } from "react"
-import socketIOClient from "socket.io-client"
 import OrderRow from './OrderRow'
-
 
 class OrderTable extends Component {
   constructor() {
     super()
     this.state = {
-      response: false,
-      endpoint: "http://127.0.0.1:8080",
-      orders: {},
       exchanges: ["Bittrex", "Poloniex"]
     };
   }
-
-  // componentDidMount() {
-  //   const { endpoint } = this.state
-  //   const socket = socketIOClient(endpoint)
-  //   let index = 0
-  //   if(this.props.type === "asks"){index = 1}
-  //   socket.on("orders", data => this.setState({ orders: data[index] }))
-  // }
   render() {
     const orderList = this.props.orders && Object.keys(this.props.orders).map((price) => {
       return <OrderRow price={price} volumes={this.props.orders[price]} />
     })
-
 
     const exchangesList = this.state.exchanges.map((exchange) => {
       return <th scope="col">{exchange}</th>

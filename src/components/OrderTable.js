ed086c6c5eef9a8165653e93469a44e708aac3c9
Ellen Cooper <ellencooper74@gmail.com>
2017-11-04 20:32:14 -0600

    added dynamic generation ot table headers

--- src/components/OrderTable.js
+++ src/components/OrderTable.js
@@ -9,20 +9,22 @@ class OrderTable extends Component {
     };
   }
   render() {
+    let firstPrice = this.props.orders && Object.keys(this.props.orders)[0]
+    const exchangesList = this.props.orders && Object.keys(this.props.orders[firstPrice]["volumes"]).map((exchangeName) => {
+      return <th scope="col">{exchangeName}</th>
+    })
+
     const orderList = this.props.orders && Object.keys(this.props.orders).map((price) => {
       return <OrderRow price={price} volumes={this.props.orders[price]["volumes"]} highlight={this.props.orders[price]["highlight"]} />
     })
 
-    const exchangesList = this.state.exchanges.map((exchange) => {
-      return <th scope="col">{exchange}</th>
-    })
 
     return (
     <table class="table table-sm">
       <thead>
         <tr>
-          <th scope="col"></th>
-          <th class="text-center" colspan="3" scope="col">Volume</th>
+          <th scope="col" class="no-top-border"></th>
+          <th class="text-center" colspan="12" scope="col">Volume</th>
         </tr>
         <tr>
           <th scope="col">Price (BTC)</th>

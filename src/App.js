725b0e751aa45de1977c9328f93103c3a33d5428
Ellen Cooper <ellencooper74@gmail.com>
2017-11-05 23:43:00 -0700

    changes font

--- src/App.js
+++ src/App.js
@@ -44,8 +44,8 @@ class App extends Component {
       <div class="container-fluid text-center">
         <h1>BTC-{this.state.market} Combined Order Books</h1><br></br>
         <h5>Select Market: {this.renderMarketDropdown()}</h5><br></br>
-        <div class="col-md-10 offset-md-1 text-center">
-          <OrderAreaChart orders={this.state.orders} market={this.state.market}/>
+        <div class="col-md-10 offset-md-1 text-center" alt="chart">
+          <OrderAreaChart orders={this.state.orders} market={this.state.market} />
         </div>
         <div class="col-md-6 offset-md-3"><br></br><br></br>
           <h5 class = "text-center font-italic bg-warning">Highlighting Represents Overlapping Asks/Bids</h5><br></br>

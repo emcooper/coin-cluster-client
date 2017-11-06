a2712b241a8b943881836c769a4f6c3aaf89adca
Ellen Cooper <ellencooper74@gmail.com>
2017-11-05 22:26:35 -0700

    deleted unused code

--- src/components/OrderAreaChart.js
+++ src/components/OrderAreaChart.js
@@ -8,16 +8,13 @@ class OrderAreaChart extends Component {
   }
 
   render(){
-    let data = []
-    let data2 =[]
+    let data =[]
 
     if(this.props.orders[0]){
       let bids = this.props.orders[0]
       let asks = this.props.orders[1]
       let min = + this.roundIncrement(Object.keys(bids).slice(-1)[0])
       let max = + Object.keys(asks).slice(-1)[0]
-      let increment = (max - min) /20
-      console.log(increment)
       let smoothedData = {}
       let i = min
       while(i <= max + .0005){
@@ -42,28 +39,14 @@ class OrderAreaChart extends Component {
         smoothedData[roundedPx]['ask'] += totalVol
       })
 
-    data2 = Object.keys(smoothedData).map(function(price){
-      return {price: +price, bid: smoothedData[price]['bid'], ask: smoothedData[price]['ask']}
-    })
-  }
+      data = Object.keys(smoothedData).map(function(price){
+        return {price: +price, bid: smoothedData[price]['bid'], ask: smoothedData[price]['ask']}
+      })
+    }
 
-    data.sort(function (a, b) {
-      return a.price - b.price
-    })
-    //
-    // const data1 = [
-    //   {price: .03, bid: 4000, ask: 0},
-    //   {price: .04, bid: 3000, ask: 0},
-    //   {price: .05, bid: 3000, ask: 0},
-    //   {price: .06, bid: 2000, ask: 0},
-    //   {price: .075, bid: 9000},
-    //   {price: .08, bid: 0, ask: 4800},
-    //   {price: .09, ask: 3800},
-    //   {price: .1, ask: 4300},
-    // ]
     return(
 
-      <AreaChart width={1000} height={250} data={data2}
+      <AreaChart width={1000} height={250} data={data}
         margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
         <defs>
           <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">

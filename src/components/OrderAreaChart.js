be5b07be126c91bd5202d57b9ecd20fec28f85ff
Ellen Cooper <ellencooper74@gmail.com>
2017-11-05 21:54:35 -0700

    completes data smoothing

--- src/components/OrderAreaChart.js
+++ src/components/OrderAreaChart.js
@@ -3,40 +3,57 @@ import { AreaChart, XAxis, YAxis, CartesianGrid, Tooltip, Area } from 'recharts'
 
 class OrderAreaChart extends Component {
 
+  roundIncrement(price){
+    return +(Math.round(price*2000)/2000).toFixed(4)
+  }
 
   render(){
     let data = []
+    let data2 =[]
 
     if(this.props.orders[0]){
       let bids = this.props.orders[0]
       let asks = this.props.orders[1]
-      let min = + Object.keys(bids).slice(-1)[0]
+      let min = + this.roundIncrement(Object.keys(bids).slice(-1)[0])
       let max = + Object.keys(asks).slice(-1)[0]
       let smoothedData = {}
-      for(let i = min; i <= max; i += .0005){
-        smoothedData[i] = {'bid': 0, 'ask': 0}
+      // for(let i = min; i <= max; i += .0005){
+      //   smoothedData[i] = {'bid': 0, 'ask': 0}
+      // }
+      let i = min
+      while(i <= max + .0005){
+        smoothedData[this.roundIncrement(i)] = {'bid': 0, 'ask': 0}
+        i = +this.roundIncrement(i) + .0005
       }
 
       Object.keys(bids).forEach(function(price){
         let totalVol = 0
+        let roundedPx = +(Math.ceil(price*2000)/2000).toFixed(4)
         Object.keys(bids[price].volumes).forEach((exchangeName) => {
           totalVol += bids[price].volumes[exchangeName]
         })
-        data.push({price: + price, bid: totalVol})
+        smoothedData[roundedPx]['bid'] += totalVol
       })
 
       Object.keys(asks).forEach(function(price){
         let totalVol = 0
+        let roundedPx = +(Math.ceil(price*2000)/2000).toFixed(4)
+
         Object.keys(asks[price].volumes).forEach((exchangeName) => {
           totalVol += asks[price].volumes[exchangeName]
         })
-        data.push({price: + price, ask: totalVol})
+        smoothedData[roundedPx]['ask'] += totalVol
       })
-    }
+
+    data2 = Object.keys(smoothedData).map(function(price){
+      return {price: +price, bid: smoothedData[price]['bid'], ask: smoothedData[price]['ask']}
+    })
+  }
 
     data.sort(function (a, b) {
       return a.price - b.price
     })
+    console.log(data2)
     //
     // const data1 = [
     //   {price: .03, bid: 4000, ask: 0},
@@ -48,10 +65,9 @@ class OrderAreaChart extends Component {
     //   {price: .09, ask: 3800},
     //   {price: .1, ask: 4300},
     // ]
-    console.log(data)
     return(
 
-      <AreaChart width={1000} height={250} data={data}
+      <AreaChart width={1000} height={250} data={data2}
         margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
         <defs>
           <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
